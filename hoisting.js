6f23a1f9caefafbde60f82ec33cef1d103262fcf
Joanna White <joanna1016@hotmail.com>
2019-03-02 23:40:30 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -19,11 +19,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
+
+  sayMy();
 
 };
